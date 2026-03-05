912fce88ea53b12d48335d99b01af26f010b0ee9
Kayyo <Sullyman08@gmail.com>
2026-03-05 13:17:30 -0500

    2.0

--- src/core/common.h
+++ src/core/common.h
@@ -1,24 +1,65 @@
-#ifndef COMMON_H
-#define COMMON_H
+#ifndef CommonH
+#define CommonH
 
-#include <stdint.h>
-#include <stddef.h>
+#define Version "0.1.0"
 
 enum {
-	True = 1,
-	False = 0,
+    True = 1,
+    False = 0,
+
+    Ok = 0,
+    Err = 1,
 };
 
+typedef unsigned char boolean; 
+typedef unsigned char result;
+typedef unsigned long usize;
+
 typedef struct {
-	char **argv;
-	int argc;
-} program_arguments_t;
+    char *title;
 
-extern program_arguments_t program_arguments;
+    usize argc;
+    char **argv;
+} Program;
 
-typedef uint8_t boolean;
-typedef size_t usize;
+typedef struct {
+    void *pointer;
+    usize size;
+    void *priv;
+} Heap;
 
-void *null(void);
+extern void *const Null;
+extern Program program;
+extern const Heap NullHeap;
 
+void quit(result res);
+
+result open_logger(void);
+result close_logger(void);
+
+void log_msg(const char *fmt, ...);
+void log_warn(const char *fmt, ...);
+void log_err(const char *fmt, ...);
+
+usize get_error_count(void);
+usize get_warn_count(void);
+
+#ifdef Testing
+void restore_diagnostic_counts(usize warn_count, usize error_count);
+#endif // Testing
+
+Heap allocate(usize count, usize bytes);
+Heap reallocate(Heap heap, usize new_size);
+void deallocate(Heap heap);
+
+result scan_and_deallocate(void); // returns Err if any leaked bytes are reclaimed, otherwise Ok
+
+void set_default_raylib_log_level(void);
+
+#if !defined(CommonAllowStdlibAllocators)
+#define malloc(...) UseAllocateInsteadOfMalloc
+#define realloc(...) UseReallocateInsteadOfRealloc
+#define free(...) UseDeallocateInsteadOfFree
 #endif
+
+#endif // COMMON_H
--- src/main.c
+++ src/main.c
@@ -1,24 +1,7 @@
-#include "core/allocator.h"
-#include "core/logger.h"
 #include "core/common.h"
 
-enum {
-	ProgramMaxAllocatedBytes = 4096,
-};
+int main(void) {
+	log_msg("Hello, world!");
 
-void program(void) {
-	//...
-}
-
-int main(int argc, char **argv) {
-	init_vdisk(ProgramMaxAllocatedBytes);
-
-	program_arguments.argc = argc;
-	program_arguments.argv = argv;
-
-	program();
-
-	free_vdisk();
-
-	return had_error(); 
+	quit(Ok);
 }
